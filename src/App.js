84576aa0991183e0db03ac3f3ec87ab580c0ba71
KcobatiRabiaa <rabiaakochbati@gmail.com>
2021-08-07 13:51:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React , {Component} from 'react'
+
+
+class App extends Component {
+ constructor(props){
+   console.log("constructor() ")
+   super(props)
+   this.state = {
+     person: [
+           { fullName:'Kochbati Rabiaa',
+             bio:'A master degree in analytical chemistry from the University of Tunis: Faculty of Sciences of Tunis.A wife and a mother of a beautiful twin',
+             profession:'chemistry teacher',
+             imgSrc: require ("../src/photo1.jpg")
+            }
+          ]
+            ,
+    isVisible : false,
+    interval: null ,
+    timer : 0,
+  };
 }
+componentDidMount () {
+ console.log("componentDidMount() ");
+ this.setState({
+  interval: setInterval(() => {
+    this.setState ({timer : this.state.timer +1})
+  }, 1000)
+})
+}
+
+
+componentDidUpdate(){
+  console.log("componentDidUpdate()")
+  if(!this.state.isVisible)
+  
+  {
+    this.state.timer=0
+  }
+}
+
+componentWillUnmount(){
+  console.log("componentWillUnmount()")
+  if (this.state.isVisible){
+ clearInterval(this.state.interval)}
+}
+
+toggleVisibility=()=> {
+  this.setState({
+    isVisible: !this.state.isVisible
+  })
+ }
+
+
+ render(){  
+    console.log("render() ")
+   return (
+      <div>
+
+      <button className='btn' onClick={this.toggleVisibility}> {this.state.isVisible ? 'hide the profile' : 'show the profile'}</button>
+      {this.state.isVisible ? 
+      (
+       <div>    
+         {this.state.person.map((person, index) => {
+          return (
+            
+              <div key={index}>
+                  <p>Full Name:{person.fullName}</p>
+                  <p>Bio:{person.bio}</p>
+                  <p>Profession:{person.profession}</p>
+                  <img src ={person.imgSrc} alt="" />
+              </div>
+          )})
+           
+      }
+      
+     <h4>{this.state.timer}</h4>
+      </div>) :(<h4>Clic on the button to show the profile</h4>)
+      
+      }                                                        
+     
+     </div>
+    )
+  }
+ }
+ 
 
 export default App;
